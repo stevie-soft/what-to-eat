85360f7f3b768a29e53121d57120d1b7daa34b30
stevie-soft <buzas.programming@gmail.com>
2025-06-10 21:17:41 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module david.buzas.whattoeat {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens david.buzas.whattoeat to javafx.fxml;
+    exports david.buzas.whattoeat;
+}
