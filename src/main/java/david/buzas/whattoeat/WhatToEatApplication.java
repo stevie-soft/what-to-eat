04564ec7c076f10d88a641a4d6211e7f4c4d2344
stevie-soft <buzas.programming@gmail.com>
2025-06-10 21:37:05 +0200

    resources | hello-view.fxml | rename to main-view.fxml

--- src/main/java/david/buzas/whattoeat/WhatToEatApplication.java
+++ src/main/java/david/buzas/whattoeat/WhatToEatApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class WhatToEatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WhatToEatApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(WhatToEatApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setFullScreen(true);
         stage.setTitle("Mit együnk ma?");
