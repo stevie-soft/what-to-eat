887a6e2cd3d7a7693bcad50595c71e40a279353c
stevie-soft <buzas.programming@gmail.com>
2025-06-13 21:38:25 +0200

    add ItemManager & implement JsonFileItemManager

---
+++ src/main/java/david/buzas/whattoeat/ItemManager.java
@@ -0,0 +1,9 @@
+package david.buzas.whattoeat;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ItemManager<TItem> {
+    List<TItem> readItems() throws IOException;
+    void writeItems(List<TItem> items) throws IOException;
+}
